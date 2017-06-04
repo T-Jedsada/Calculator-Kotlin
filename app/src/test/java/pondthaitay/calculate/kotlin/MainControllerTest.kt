bce58dca0e428cf2667fe0a68f545291f13696f9
Jedsada Tiwongvorakul <jt@20scoops.com>
2017-06-04 16:40:48 +0700

    kotlin unittest.

--- app/src/test/java/pondthaitay/calculate/kotlin/MainControllerTest.kt
+++ app/src/test/java/pondthaitay/calculate/kotlin/MainControllerTest.kt
@@ -1,5 +1,7 @@
 package pondthaitay.calculate.kotlin
 
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
@@ -7,28 +9,52 @@ import org.mockito.MockitoAnnotations
 
 class MainControllerTest {
 
-    @Mock lateinit var calculate: Calculator
+    @Mock
+    private var mockView: MainView? = null
+
+    private var controller: MainController? = null
+    private var calculator: Calculator? = null
 
     @Before
+    @Throws(Exception::class)
     fun setUp() {
         MockitoAnnotations.initMocks(this)
+        calculator = Calculator.instance
+        controller = MainController(mockView!!, calculator!!)
     }
 
     @Test
+    @Throws(Exception::class)
     fun plus() {
-
+        controller?.plus(5,5)
+        verify(mockView, times(1))?.setResultCalculate("10")
     }
 
     @Test
+    @Throws(Exception::class)
     fun minus() {
+        controller?.minus(5,5)
+        verify(mockView, times(1))?.setResultCalculate("0")
     }
 
     @Test
+    @Throws(Exception::class)
     fun multiply() {
+        controller?.multiply(5,5)
+        verify(mockView, times(1))?.setResultCalculate("25")
     }
 
     @Test
+    @Throws(Exception::class)
     fun divide() {
+        controller?.divide(5,5)
+        verify(mockView, times(1))?.setResultCalculate("1")
     }
 
+    @Test
+    @Throws(Exception::class)
+    fun divideByZero() {
+        controller?.divide(5,0)
+        verify(mockView, times(1))?.setResultCalculate("DivideByZeroException")
+    }
 }
